79d2b2bb17e1dcfb6e3cda4cd1572988abfed56c
kikeanast <enrianastasini@gmail.com>
2023-04-15 15:37:39 -0300

    BaSP-W05-2023: added 2nd js excercise

---
+++ week-05/excercises/02-strings.js
@@ -0,0 +1,17 @@
+/* Ejercicio a) */
+
+var textoA = "lorem ipsum dolor sit amet";
+var textoMayuscula = textoA.toUpperCase();
+console.log(textoMayuscula);
+
+/* Ejercicio b) */
+
+var textoB = "lorem ipsum dolor sit amet";
+var textoB5 = textoB.substring(0, 5);
+console.log(textoB5);
+
+/* Ejercicio c) */
+
+var textoC = "lorem ipsum dolor sit amet";
+var toC = textoC.substring(textoC.length - 3);
+console.log(toC);
