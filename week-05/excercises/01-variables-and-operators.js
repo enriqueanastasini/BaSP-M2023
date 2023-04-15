b6234484adfdaae3622869fee57a363698ece5f2
kikeanast <enrianastasini@gmail.com>
2023-04-15 14:45:30 -0300

    BaSP-W05-2023: Creation of week05 folders and .js files, added 1st excercise of js

---
+++ week-05/excercises/01-variables-and-operators.js
@@ -0,0 +1,21 @@
+/* Ejercicio a) */
+var a = 5;
+var b = 7;
+var c = a + b;
+console.log(c);
+
+
+/* Ejercicio b) */
+
+var nombre = "Enrique";
+var apellido = "Anastasini";
+var nombreApellido = nombre + " " + apellido;
+console.log(nombreApellido);
+
+/* Ejercicio c) */
+
+var palabra1 = "Radium";
+var palabra2 = "Rocket";
+var sumaPalabras = palabra1.length + palabra2.length;
+console.log(sumaPalabras);
+
